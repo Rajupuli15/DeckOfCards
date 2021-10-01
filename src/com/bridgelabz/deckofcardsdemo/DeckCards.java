3564f1f34c7dfa891cdea4c99f08e13bf11b2f90
Rajupuli15 <Rajupuli15@gmail.com>
2021-10-01 00:17:29 -0400

    Added:deckcards

--- src/com/bridgelabz/deckofcardsdemo/DeckCards.java
+++ src/com/bridgelabz/deckofcardsdemo/DeckCards.java
@@ -7,7 +7,7 @@ public class DeckCards {
                             //create method and add in arraylist
         public void DeckCards() {
             String[] suit = {"Clubs,Diamonds,Hearts,Spade,Queen"};
-            String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+            String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King", "Ace"};
             for (int i = 0; i < rank.length; i++) {
                 for (int j = 0; j < suit.length; j++) {
                     arrayList.add(rank[i]+" "+suit[i]);
--- src/com/bridgelabz/deckofcardsdemo/MainDeckofcard.java
+++ src/com/bridgelabz/deckofcardsdemo/MainDeckofcard.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.deckofcardsdemo;
 
-public class Deckofcard {
+public class MainDeckofcard {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Deckofcards");
